8b1fc8a84e2f46b994c5e8ece3e6f1342a48cd19
EstefiBV <estefania.boza@uned.cr>
2026-03-26 18:04:56 -0600

    Agregar archivos de proyecto.

---
+++ WebForms/Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
